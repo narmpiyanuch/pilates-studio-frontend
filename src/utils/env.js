da1dbb710ff0b3adfe6ba9dcb7f38739d3eb81c5
narmpiyanuch <a.piyanuch@hotmail.com>
2023-11-29 15:27:35 +0700

    update-backend-url

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,1 +1,3 @@
-export const BACKEND_URL = 'http://localhost:8880'
+export const BACKEND_URL = "https://pilates-studio-backend-q3eh.vercel.app/";
+
+//export const BACKEND_URL = 'http://localhost:8880'
